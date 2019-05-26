ca24a6294d15c4a4c4a1f04a3150db801739b5d1
spadeloves <new@example.com>
2019-05-27 00:27:09 +0900

    TODO詳細の動的表示まで

--- components/TODODetail.vue
+++ components/TODODetail.vue
@@ -1,5 +1,30 @@
 <template>
-  <section class="container">
-    <h1>TodoDetail</h1>
+  <section>
+    <b-row>
+      <h2>{{todoData[activeCategory-1].todos[activeTodo].title}}</h2>
+    </b-row>
+    <b-row>
+      <b-col>
+        <p><i class="fas fa-folder-open"></i> {{todoData[activeCategory-1].category_name}}</p>
+      </b-col>
+      <b-col>
+        <p><i class="fas fa-calendar-day"></i> {{todoData[activeCategory-1].todos[activeTodo].deadline}}</p>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col>
+        <p>{{todoData[activeCategory-1].todos[activeTodo].note}}</p>
+      </b-col>
+    </b-row>
   </section>
 </template>
+
+
+<script>
+export default {
+  name: 'TODODetail',
+  props:['todoData','activeTodo','activeCategory','selectedTODO','categoryName'],
+  methods: {
+  },
+}
+</script>
