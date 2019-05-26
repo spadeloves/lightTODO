2af3932f3ff3cae7cacdc929537e67cb7d6ad481
SpadeLoves <spadeloves@gmail.com>
2019-05-26 17:14:23 +0900

    レイアウト修正、Category一覧の骨組作成
    ・レイアウトを全体的に修正
    ・セクションタイトルをコンポーネント化
    ・Category一覧の骨組み作成

--- components/CategoryList.vue
+++ components/CategoryList.vue
@@ -1,6 +1,33 @@
 <template>
-  <section class="container">
-    <h1>CategoryList</h1>
-  </section>
+    <b-row>
+		<b-col>
+			<ul>
+				<li v-for='(category,index) in categoryList' :key='index'>
+					{{category.name}}
+				</li>
+			</ul>
+		</b-col>
+	</b-row>
 </template>
 
+<script>
+export default {
+  	name: 'CategoryList',
+  	data: function() {
+  		return {	
+		    categoryList: [
+		    	{
+			  		category_id: 1,
+			  		name: "Category1",
+			  		color:"#11ff11"
+				},
+				{
+			  		category_id: 2,
+			  		name: "Category2",
+			  		color:"#ff1143"
+				},
+		    ]
+		}
+	}
+}
+</script>
---
+++ components/SectionHeader.vue
@@ -0,0 +1,14 @@
+<template>
+	<b-row>
+		<b-col>
+			<h1>{{title}}</h1>
+		</b-col>
+	</b-row>
+</template>
+
+<script>
+export default {
+  	name: 'SectionHeader',
+  	props:['title']	
+}
+</script>
