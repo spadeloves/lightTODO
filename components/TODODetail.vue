9dcde8d5032bbf4ef53936ffe0ec95c80d90477b
SpadeLoves <spadeloves@gmail.com>
2019-05-27 00:33:16 +0900

    TODOメモの表示を追加

--- components/TODODetail.vue
+++ components/TODODetail.vue
@@ -16,6 +16,23 @@
         <p>{{todoData[activeCategory-1].todos[activeTodo].note}}</p>
       </b-col>
     </b-row>
+    <b-row>
+      <b-col>
+        <b-list-group>
+          <b-list-group-item 
+          class="flex-column align-items-start"
+          v-for='(comment,index) in todoData[activeCategory-1].todos[activeTodo].comments' :key='index' 
+          >
+            <div class="d-flex w-100 justify-content-between">
+              <small>{{comment.date}}</small>
+            </div>
+            <p class="mb-1">
+              {{comment.content}}
+            </p>
+          </b-list-group-item>
+        </b-list-group>
+      </b-col>
+    </b-row>
   </section>
 </template>
 
