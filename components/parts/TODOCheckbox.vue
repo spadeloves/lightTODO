b9cdc26fb9fdf75629bd5e62d4b765c2dd41473b
YutaOkuzono <okuzono-yuta@systemsoft.co.jp>
2019-05-27 18:01:10 +0900

    クリックイベント追加
    とりあえずコミット

--- components/parts/TODOCheckbox.vue
+++ components/parts/TODOCheckbox.vue
@@ -1,6 +1,6 @@
 <template>
-	<i class="far fa-check-square" v-if='todoIsCompleted'></i>
-    <i class="far fa-square" v-else></i>
+	<i class="far fa-check-square" v-if='todoIsCompleted' v-on:click="changeTODOStatus(false)"></i>
+    <i class="far fa-square" v-on:click="changeTODOStatus(true)" v-else></i>
 </template>
 
 <script>
@@ -8,6 +8,11 @@ export default {
   name: 'TODOCheckbox',
   props:['todoId','categoryId','todoIsCompleted'],
   methods: {
+  	changeTODOStatus:function(status){
+  		console.log(this.todoIsCompleted);
+  		this.todoIsCompleted = status;
+  		console.log(this.todoIsCompleted);
+  	}
   },
 }
 </script>
