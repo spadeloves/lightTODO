24fda21ed2c9b2b55338c659ed198f7930add8f5
SpadeLoves <spadeloves@gmail.com>
2019-05-28 21:19:04 +0900

    indexから渡していたTODOリストをstateからの渡しに置き換え

--- components/functions/TODODetail.vue
+++ components/functions/TODODetail.vue
@@ -6,23 +6,23 @@
           <TODOCheckbox 
           :todo-id="activeTodo"
           :category-id="activeCategory-1"
-          :todo-is-completed="todoData[activeCategory-1].todos[activeTodo].is_completed"
+          :todo-is-completed="$store.state.todos.todoData[activeCategory-1].todos[activeTodo].is_completed"
           ></TODOCheckbox>
-          {{todoData[activeCategory-1].todos[activeTodo].title}}
+          {{$store.state.todos.todoData[activeCategory-1].todos[activeTodo].title}}
         </h2>
       </b-col>
     </b-row>
     <b-row>
       <b-col>
-        <p><i class="fas fa-folder-open"></i> {{todoData[activeCategory-1].category_name}}</p>
+        <p><i class="fas fa-folder-open"></i> {{$store.state.todos.todoData[activeCategory-1].category_name}}</p>
       </b-col>
       <b-col>
-        <p><i class="fas fa-calendar-day"></i> {{todoData[activeCategory-1].todos[activeTodo].deadline}}</p>
+        <p><i class="fas fa-calendar-day"></i> {{$store.state.todos.todoData[activeCategory-1].todos[activeTodo].deadline}}</p>
       </b-col>
     </b-row>
     <b-row>
       <b-col>
-        <p>{{todoData[activeCategory-1].todos[activeTodo].note}}</p>
+        <p>{{$store.state.todos.todoData[activeCategory-1].todos[activeTodo].note}}</p>
       </b-col>
     </b-row>
     <b-row>
@@ -30,7 +30,7 @@
         <b-list-group>
           <b-list-group-item 
           class="flex-column align-items-start"
-          v-for='(comment,index) in todoData[activeCategory-1].todos[activeTodo].comments' :key='index' 
+          v-for='(comment,index) in $store.state.todos.todoData[activeCategory-1].todos[activeTodo].comments' :key='index' 
           >
             <div class="d-flex w-100 justify-content-between">
               <small>{{comment.date}}</small>
