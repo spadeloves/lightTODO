20fb96ac01eec1b170d0b0a59f8f972e2def6d5e
SpadeLoves <spadeloves@gmail.com>
2019-06-04 13:08:44 +0900

    カテゴリ追加の続き

--- components/functions/CategoryList.vue
+++ components/functions/CategoryList.vue
@@ -3,14 +3,15 @@
 	    <b-row>
 				<b-col>
 					<ul>
-						<b-list-group-item 
-						v-on:click="setActive" 
-						v-for='(category,index) in $store.state.todos.todoData' :key='index' 
-						:href="'#'+category.category_id" 
-						:data-categoryid="category.category_id" 
+						<b-list-group-item
+						v-on:click="setActive"
+						v-for='(category,index) in $store.state.todos.todoData' :key='index'
+						:href="'#'+category.category_id"
+						:data-categoryid="category.category_id"
 						:active="$store.state.todos.activeCategory==category.category_id">
 						<i class="fas fa-circle" v-bind:style="{color:category.color}"></i> {{category.category_name}}
 					</b-list-group-item>
+					<AddCategoryFormInList></AddCategoryFormInList>
 				</ul>
 			</b-col>
 		</b-row>
@@ -18,8 +19,13 @@
 </template>
 
 <script>
+import AddCategoryFormInList from '~/components/parts/AddCategoryFormInList.vue'
+
 export default {
 	name: 'CategoryList',
+	components:{
+		AddCategoryFormInList
+	},
 	props:['activeCategory'],
 	methods: {
 		//クリックされたCategoryを活性化する
